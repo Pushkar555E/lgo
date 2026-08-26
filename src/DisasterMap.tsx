dd9d887c3ee8848749927c0ca734b21b2f37e316
Pushkar BIswas <pushkarbiswas05@gmail.com>
2026-08-26 23:43:00 +0530

    Use CartoDB Dark Matter raster tiles to fix blank map

--- src/DisasterMap.tsx
+++ src/DisasterMap.tsx
@@ -21,7 +21,31 @@ export default function DisasterMap({ onZoneSelect }: DisasterMapProps) {
 
     const map = new maplibregl.Map({
       container: containerRef.current,
-      style: "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json",
+      style: {
+        version: 8,
+        sources: {
+          "carto-dark": {
+            type: "raster",
+            tiles: [
+              "https://a.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png",
+              "https://b.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png",
+              "https://c.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png",
+              "https://d.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png"
+            ],
+            tileSize: 256,
+            attribution: "&copy; OpenStreetMap contributors, &copy; CARTO"
+          }
+        },
+        layers: [
+          {
+            id: "carto-dark-layer",
+            type: "raster",
+            source: "carto-dark",
+            minzoom: 0,
+            maxzoom: 20
+          }
+        ]
+      },
       center: INITIAL_CENTER,
       zoom: INITIAL_ZOOM,
       pitch: 40
